206c80dca65caef9edef1092b709f7d50081b021
Aleksei <lex.gmx@gmail.com>
2020-08-31 18:01:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const object = {
+//     a:50
+// };
+
+// object.a = 100;
+// console.log(object);
+
+// const answer = prompt("Вам есть 18?", " ");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt("Как Вас зовут?");
+// answers[1] = prompt("Как Ваша фамилия?");
+// answers[2] = prompt("Сколько Вам лет?");
+
+// console.log(typeof(answers));
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Alex";
+// console.log(`Привет, ${user}!`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(incr);
+// console.log(decr--);
+// console.log(decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
